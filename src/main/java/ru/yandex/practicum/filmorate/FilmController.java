54d16dddb0a13ba0473ac304384df3adf9a0fd7a
artem <tam-artemka@mail.ru>
2025-05-22 15:32:36 +0300

    12 спринт реализовал часть замечаний

--- src/main/java/ru/yandex/practicum/filmorate/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/FilmController.java
@@ -1,9 +1,9 @@
 package ru.yandex.practicum.filmorate;
 
-import jakarta.validation.Valid;
+
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
+
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.service.FilmService;
